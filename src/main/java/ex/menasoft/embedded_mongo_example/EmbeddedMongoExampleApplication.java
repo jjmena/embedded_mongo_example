b5be2be2231bc4bee8e317f9fbd9eee618c1d8fc
Jacinto Jesús Mena Lomeña <jacintomena@gmail.com>
2019-02-22 23:06:31 +0100

    initial commit

---
+++ src/main/java/ex/menasoft/embedded_mongo_example/EmbeddedMongoExampleApplication.java
@@ -0,0 +1,13 @@
+package ex.menasoft.embedded_mongo_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedMongoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedMongoExampleApplication.class, args);
+	}
+
+}
